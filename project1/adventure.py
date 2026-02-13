5924aac670eea1ae8e2b92c094d01cff8113ea6f
jay0708 <mupin0708@gmail.com>
2026-02-13 02:05:23 -0500

    asdf

--- project1/adventure.py
+++ project1/adventure.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
 
     game_log = EventList()  # This is REQUIRED as one of the baseline requirements
     game = AdventureGame('project1/game_data.json', 1)  # load data, setting initial location ID to 1
-    menu = ["look", "inventory", "score", "log", "quit"]  # Regular menu options available at each location
+    menu = ["look", "inventory", "score", "log", "quit"]  # Regular menu options available at each location 1
     choice = None
 
     print("You wake up in a panic. Your CS project is due at 1pm today, \n but you are missing critical items!")
