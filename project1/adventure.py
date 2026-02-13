0e7635344af0630525a797455de6493da93b4290
jay0708 <mupin0708@gmail.com>
2026-02-13 01:46:43 -0500

    asdf

--- project1/adventure.py
+++ project1/adventure.py
@@ -190,7 +190,7 @@ if __name__ == "__main__":
     })
 
     game_log = EventList()  # This is REQUIRED as one of the baseline requirements
-    game = AdventureGame('/Users/jay0708/PycharmProjects/csc111/project1/game_data.json',
+    game = AdventureGame('project1/adventure.py',
                          1)  # load data, setting initial location ID to 1
     menu = ["look", "inventory", "score", "log", "quit"]  # Regular menu options available at each location
     choice = None
